6eaac4401d4708506e1f2e82422339b6f97162a7
Keshav Sharma <keshavsharma@Keshavs-MacBook-Air.local>
2026-05-29 22:16:19 +0530

    render some change

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r eventify_backend/requirements.txt
+cd eventify_backend
+
 python manage.py collectstatic --no-input
 python manage.py migrate
